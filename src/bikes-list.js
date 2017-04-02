f60fe1c4c6ac475fdaa2e30225cca65234b6ccc6
Marek Piechut <marek.piechut@gmail.com>
2017-04-02 11:40:55 +0200

    ex3

--- src/bikes-list.js
+++ src/bikes-list.js
@@ -3,15 +3,7 @@ import _ from 'lodash'
 import Bike from './bikes-list-element'
 
 const BikesList = ({bikes}) => {
-	const renderBike = (b) => (
-		<Bike bike={b} key={b.id}/>
-	)
-
-	return (
-		<ul className='bikes-list'>
-			{_.map(bikes, renderBike)}
-		</ul>
-	)
+	return <span>YOUR CODE GOES HERE</span>
 }
 
 export default BikesList
