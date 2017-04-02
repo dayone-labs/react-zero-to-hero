30976a5929b82d8c66f02593df52355c92e04762
Marek Piechut <marek.piechut@gmail.com>
2017-04-02 11:50:04 +0200

    ex4final

--- src/main.js
+++ src/main.js
@@ -1,18 +1,14 @@
 import React from 'react'
 import uuid from 'uuid/v4'
 import _ from 'lodash'
-import logLifecycle from './log-lifecycle'
 import BikesList from './bikes-list'
 import BikeDetails from './bike-details'
-import Modal from './modal'
-import AddNew from './add-new'
 import Banner from './banner'
 import {bikes} from './data'
 
 const Main = React.createClass({
 	getInitialState() {
 		return {
-			bikes,
 			selectedBike: bikes[0]
 		}
 	},
@@ -23,28 +19,8 @@ const Main = React.createClass({
 		})
 	},
 
-	showNewPopup() {
-		this.setState({
-			showNewPopup: true
-		})
-	},
-
-	hideNewPopup() {
-		this.setState({
-			showNewPopup: false
-		})
-	},
-
-	createNew(bikeData) {
-		const bike = _.assign({id: uuid()}, bikeData)
-		this.setState({
-			bikes: _.concat(this.state.bikes, bike),
-			showNewPopup: false
-		})
-	},
-
 	render() {
-		const { bikes, selectedBike, showNewPopup } = this.state
+		const { selectedBike } = this.state
 
 		return (
 			<div>
@@ -52,12 +28,6 @@ const Main = React.createClass({
 				<div className='container-fluid'>
 					<div className='row fill-height'>
 						<div className='col-md-4 grey-panel pb-3'>
-							<hr/>
-							<div className='text-center'>
-								<button onClick={this.showNewPopup}
-									className='btn btn-danger'>Add new</button>
-							</div>
-							<hr/>
 							<BikesList
 								onSelect={this.selectBike}
 								bikes={bikes} />
@@ -66,9 +36,6 @@ const Main = React.createClass({
 							<BikeDetails bike={selectedBike} />
 						</div>
 					</div>
-					<Modal visible={showNewPopup} onDismiss={this.hideNewPopup}>
-						<AddNew onSave={this.createNew} onCancel={this.hideNewPopup}/>
-					</Modal>
 				</div>
 			</div>
 		)
